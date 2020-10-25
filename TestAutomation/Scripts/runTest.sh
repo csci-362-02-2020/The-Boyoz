e9b8cbf83d2b32e722f5a2263a4ce617c5cf497d
Nathan <nathanthebellboy@gmail.com>
2020-10-25 16:01:09 -0400

    More work on the script

--- TestAutomation/Scripts/runTest.sh
+++ TestAutomation/Scripts/runTest.sh
@@ -9,7 +9,7 @@ javac TestCaseExecutables/*.java
 for file in TestCases/*.txt
 	do
 		i=0
-	echo $file read
+	#echo $file read
 while read line
 	do
 		lines[$i]="$line";
@@ -22,19 +22,60 @@ while read line
 	declare method=${lines[3]}
 	declare input=${lines[4]}
 	declare expectedOutput=${lines[5]}
-	#declare output
+	declare output
 	
-	echo $id
-	echo $requirement
-	echo $class
-	echo $method
-	echo $input
-	echo $expectedOutput
-	echo 
+	#echo $id
+	#echo $requirement
+	#echo $class
+	#echo  method to test = $method
+	#echo $input
+	#echo expected output = $expectedOutput
+	#echo 
 
-	sleep 1
+	#sleep 1
+	cd TestCaseExecutables
 
-	#java $method $input
-
+	if [ $method == "rgb2Hsl" ]
+		then
+			echo $id
+			output=$(java rgb2HslDriver $input)
+			#echo $output
+	
+	elif [ $method == "hex2Rgb" ]
+		then
+			echo $id
+			output=$(java hex2RgbDriver $input)
+			#echo $output
+	
+	elif [ $method == "calculate" ]
+		then
+			echo $id
+			output=$(java calculateDriver $input)
+			#echo $output
+			
+	elif [ $method == "distanceColor" ]
+		then
+			echo $id
+			output=$(java distanceColorDriver $input)
+			#echo $output
+			
+	elif [ $method == "getContrastRatio" ]
+		then
+			echo $id
+			output=$(java getContrastRatioDriver $input)
+			#echo $output
+	fi
+	
+	
+	#Check to see if test passed or failed
+	if [ "$output" == "$expectedOutput" ]
+		then
+			echo pass
+		else 
+			echo fail
+	fi
+	
+	cd ..
+	
 	done
 	
